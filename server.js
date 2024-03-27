0aa7872bff0df7a04b25c233c3194d3ce010d315
Tim Van Deusen <145621518+timvandeusen@users.noreply.github.com>
2024-03-26 22:29:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+
+
+const categoryRoutes = require('./routes/category-routes'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+app.use('/categories', categoryRoutes); // Use categoryRoutes middleware for '/categories' endpoint
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
